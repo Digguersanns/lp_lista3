740ef1ce6c82ec58e15de1b08ede14087127c75a
Digguersanns <leoapradopower2@gmail.com>
2018-11-23 22:27:10 -0200

    Ex1 L3

---
+++ lp_lista3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lp_lista3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             /* escreva um programa que armazene o valor 999 na variavel A e o valor 
+             555 na variavel B. A seguir troque os seus conteudos fazendo com que o 
+             valor que estaem A passe para B e vice e versa utilizando uma variavel C 
+             para realizar a troca. */
+
+            int A = 999;
+            int B = 555;
+            int C = 0;
+            Console.WriteLine("Exercicio 1");
+            Console.WriteLine("--------");
+            Console.WriteLine("A==>" + A );
+            Console.WriteLine("B==>" + B );
+            Console.WriteLine("C==>" + C );
+            Console.WriteLine("== TROCA ==");
+            C = A;
+            A = B;
+            B = C;
+            Console.WriteLine("A==>" + A );
+            Console.WriteLine("B==>" + B );
+            Console.WriteLine("C==>" + C );
+        }
+    }
+}
